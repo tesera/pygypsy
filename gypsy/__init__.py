bf671904f23460d1a1d6278603881ad94c4e3d62
Jotham Apaloo <jotham.apaloo@tesera.com>
2016-06-22 17:08:35 -0700

    Put data constant in top of package

--- gypsy/__init__.py
+++ gypsy/__init__.py
@@ -1,6 +1,9 @@
 """teskelypy package
 """
+import os
 from .skel import has_legs
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DATA_DIR = os.path.join(BASE_DIR, 'data')
 
-__all__ = ['has_legs']
+__all__ = ['DATA_DIR']
