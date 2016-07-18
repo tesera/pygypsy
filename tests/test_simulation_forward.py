cca7bdf627dbf191454167dc2fda80a88d6c3431
jbmsamba <julianno.sambatti@tesera.com>
2016-07-18 14:27:37 -0700

    INPUT FILES

--- tests/test_simulation_forward.py
+++ tests/test_simulation_forward.py
@@ -6,24 +6,27 @@ from gypsy import DATA_DIR
 from gypsy.forward_simulation import simulate_forwards_df
 from gypsy.GypsyDataPrep import dataPrepGypsy
 
-#
+
 #def test_forward_simulation():
-#    data_file_name = 'stands2.csv'
+#    data_file_name = '1841096_raw.csv'
 #    plot_data = pd.read_csv(os.path.join(DATA_DIR, data_file_name))
 #
 #    fplotSim = dataPrepGypsy(plot_data)[0]
 #    inputDF = pd.DataFrame(fplotSim)
 #    inputDF = inputDF.transpose()
-#
-#
-#    assert simulate_forwards_df(inputDF, simulation_choice='no')
-#    assert simulate_forwards_df(inputDF, simulation_choice='yes')
+#    
+#    inputDF.to_csv (os.path.join(DATA_DIR, 'output', '1841096.csv' ))
+    
+
+
+    #assert simulate_forwards_df(inputDF, simulation_choice='no')
+    #assert simulate_forwards_df(inputDF, simulation_choice='yes')
 
 
 def test_compare_forward_simulation():
-    data_file_name = '200863.csv'
+    data_file_name = '508159.csv'
     inputDF = pd.read_csv(os.path.join(DATA_DIR, data_file_name))
     
-    result = simulate_forwards_df(inputDF, simulation_choice='yes')
+    result = simulate_forwards_df(inputDF, simulation_choice='no')
     assert type(result) == pd.DataFrame
     result.to_csv (os.path.join(DATA_DIR, 'output', 'comparisons.csv' ))
