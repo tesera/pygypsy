eb1094ba570c8bfc8f5d5c0e224da10c0925db42
Jotham Apaloo <jotham.apaloo@tesera.com>
2016-08-31 16:17:23 -0700

    don't use X windows so that tests run in docker
    closes #8

--- gypsy/__init__.py
+++ gypsy/__init__.py
@@ -2,6 +2,10 @@
 """
 import os
 from .skel import has_legs
+import matplotlib
+
+# Force matplotlib to not use any Xwindows backend so that docker works
+matplotlib.use('Agg')
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATA_DIR = os.path.join(BASE_DIR, 'data')
