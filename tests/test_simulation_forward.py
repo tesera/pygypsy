f81fcb9bfa02db1d82caffec29b4a08698213621
Jotham Apaloo <jotham.apaloo@tesera.com>
2016-10-13 16:18:18 -0700

    Ensure we are only testing csv files

--- tests/test_simulation_forward.py
+++ tests/test_simulation_forward.py
@@ -1,4 +1,5 @@
 import os
+from glob import glob
 import pytest
 import pandas as pd
 
@@ -7,27 +8,32 @@ from gypsy.forward_simulation import simulate_forwards_df
 from gypsy.GYPSYNonSpatial import save_plot
 
 
-TEST_FILES = os.listdir(os.path.join(DATA_DIR, 'forward_simulation_files'))
+TEST_FILES = glob(os.path.join(DATA_DIR, 'forward_simulation_files', '*.csv'))
 TEST_FILES = [(item) for item in TEST_FILES]
-CHART_FILES = os.listdir(os.path.join(DATA_DIR, 'output'))
+CHART_FILES = glob(os.path.join(DATA_DIR, 'output', '*.csv'))
 CHART_FILES = [(item) for item in CHART_FILES]
+
 
 @pytest.mark.parametrize("test_file", TEST_FILES)
 def test_compare_forward_simulation(test_file):
-    data_file_name = test_file
-    inputDF = pd.read_csv(os.path.join(DATA_DIR, 'forward_simulation_files',
-                                       data_file_name))
+    input_df = pd.read_csv(test_file)
 
-    plotID = str(int(inputDF.loc[0, 'PlotID']))
-    result = simulate_forwards_df(inputDF, simulation_choice='yes')[plotID]
+    plotID = str(int(input_df.loc[0, 'PlotID']))
+    result = simulate_forwards_df(input_df, simulation_choice='yes')[plotID]
     assert type(result) == pd.DataFrame
 
-    result.to_csv(os.path.join(DATA_DIR, 'output',
-                               'comparisons_{}'.format(test_file)))
+    result.to_csv(
+        os.path.join(
+            DATA_DIR, 'output',
+            'comparisons_{}'.format(os.path.basename(test_file))
+        )
+    )
 
 @pytest.mark.parametrize("chart_file", CHART_FILES)
 def test_plot(chart_file):
-    chart_DF = pd.read_csv(os.path.join(DATA_DIR, 'output', chart_file))
+    chart_df = pd.read_csv(chart_file)
     outputfile = os.path.splitext(chart_file)[0] + '.png'
-    figure_path = os.path.join(DATA_DIR, 'figures', 'chartGR_{}'.format(outputfile))
-    assert save_plot(chart_DF, path=figure_path)
+    figure_path = os.path.join(
+        DATA_DIR, 'figures', 'chartGR_{}'.format(os.path.basename(outputfile))
+    )
+    assert save_plot(chart_df, path=figure_path)
