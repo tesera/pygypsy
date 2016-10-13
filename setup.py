bd823c76c5ada266060c93e45e470e35b0069806
Jotham Apaloo <jotham.apaloo@tesera.com>
2016-10-13 09:19:43 -0700

    Add pytest-cov and fix change requirements to >=

--- setup.py
+++ setup.py
@@ -23,14 +23,15 @@ setup(name='gypsy',
           'gypsy': ['data/*'],
       },
       install_requires=[
-          'click==6.6',
-          'pandas==0.18.1',
-          'scipy==0.17.1',
+          'click>=6.6',
+          'pandas>=0.18.1',
+          'scipy>=0.17.1',
       ],
       extras_require={
-          'test': ['pytest==2.9.1'],
-          'dev': ['pytest==2.9.1', 'sphinx==1.4.1',
-                  'pylint==1.5.4', 'git-pylint-commit-hook==2.1.1']
+          'test': ['pytest>=2.9.1'],
+          'dev': ['pytest>=2.9.1', 'sphinx>=1.4.1',
+                  'pylint>=1.5.4', 'git-pylint-commit-hook>=2.1.1',
+                  'pytest-cov>=2.3.1']
       },
       entry_points="""
       [console_scripts]
