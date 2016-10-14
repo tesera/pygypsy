c4533abf72a882a84a209146a853872d2270f019
Jotham Apaloo <jotham.apaloo@tesera.com>
2016-10-14 15:13:58 -0700

    Harden forward simulation tests
    compare results to actual data instead of just checking that it's a data frame

--- tests/test_simulation_forward.py
+++ tests/test_simulation_forward.py
@@ -1,7 +1,8 @@
 import os
-from glob import glob
 import pytest
 import pandas as pd
+from glob import glob
+import numpy.testing as npt
 
 from gypsy import DATA_DIR
 from gypsy.forward_simulation import simulate_forwards_df
@@ -10,24 +11,30 @@ from gypsy.GYPSYNonSpatial import save_plot
 
 TEST_FILES = glob(os.path.join(DATA_DIR, 'forward_simulation_files', '*.csv'))
 TEST_FILES = [(item) for item in TEST_FILES]
-CHART_FILES = glob(os.path.join(DATA_DIR, 'output', '*.csv'))
+CHART_FILES = glob(os.path.join(DATA_DIR, 'output', 'comparisons*.csv'))
 CHART_FILES = [(item) for item in CHART_FILES]
 
 
 @pytest.mark.parametrize("test_file", TEST_FILES)
 def test_compare_forward_simulation(test_file):
     input_df = pd.read_csv(test_file)
+    expected_data_path = os.path.join(
+        DATA_DIR, 'output',
+        'comparisons_{}'.format(os.path.basename(test_file))
+    )
 
     plotID = str(int(input_df.loc[0, 'PlotID']))
-    result = simulate_forwards_df(input_df, simulation_choice='yes')[plotID]
-    assert type(result) == pd.DataFrame
 
-    result.to_csv(
-        os.path.join(
-            DATA_DIR, 'output',
-            'comparisons_{}'.format(os.path.basename(test_file))
-        )
-    )
+    result = simulate_forwards_df(input_df, simulation_choice='yes')[plotID]
+    expected = pd.read_csv(expected_data_path, index_col=0)
+
+    assert type(result) == pd.DataFrame
+    assert npt.assert_almost_equal(
+        expected.values, result.values, decimal=3
+    ) is None
+
+    # regenerate output files
+    # result.to_csv(expected_data_path)
 
 @pytest.mark.parametrize("chart_file", CHART_FILES)
 def test_plot(chart_file):
