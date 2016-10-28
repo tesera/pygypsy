fab6482e754d1aa9f6259323786a585a3f710855
Jotham Apaloo <jotham.apaloo@tesera.com>
2016-10-28 16:16:03 -0700

    add config file fixture for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,17 @@
 import os
+import pytest
+
 
 DATA_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data')
+
+
+@pytest.fixture(scope='module')
+def cli_config_file():
+    path = os.path.join(DATA_DIR, 'cli_config.txt')
+
+    with open(path, 'w') as f:
+        f.write('key=value')
+
+    yield path
+
+    os.remove(path)
