95280622b2f913e67759cc0e48006330c3a8df22
Jotham Apaloo <jotham.apaloo@tesera.com>
2016-11-11 11:33:10 -0800

    Index with .at[] instead of [], remove repetition

--- gypsy/forward_simulation.py
+++ gypsy/forward_simulation.py
@@ -137,47 +137,46 @@ def simulate_forwards_df(plot_df, simulation_choice='yes'):
         plot_id = str(int(row['PlotID']))
 
         logger.info('Starting simulation for plot: %s', plot_id)
-
-        SI_bh_Aw = row['SI_Aw']
-        SI_bh_Sw = row['SI_Sw']
-        SI_bh_Pl = row['SI_Pl']
-        SI_bh_Sb = row['SI_Sb']
-        N_bh_AwT = row['N_Aw']
-        N_bh_SwT = row['N_Sw']
-        N_bh_PlT = row['N_Pl']
-        N_bh_SbT = row['N_Sb']
-        y2bh_Aw = row['y2bh_Aw']
-        y2bh_Sw = row['y2bh_Sw']
-        y2bh_Sb = row['y2bh_Sb']
-        y2bh_Pl = row['y2bh_Pl']
-        tage_AwT = row['tage_Aw']
-        tage_SwT = row['tage_Sw']
-        tage_PlT = row['tage_Pl']
-        tage_SbT = row['tage_Sb']
-        BA_AwT = row['BA_Aw']
-        BA_SwT = row['BA_Sw']
-        BA_PlT = row['BA_Pl']
-        BA_SbT = row['BA_Sb']
-        SDF_Aw0 = row['SDF_Aw']
-        SDF_Sw0 = row['SDF_Sw']
-        SDF_Pl0 = row['SDF_Pl']
-        SDF_Sb0 = row['SDF_Sb']
-        N0_Aw = row['N0_Aw']
-        N0_Sw = row['N0_Sw']
-        N0_Pl = row['N0_Pl']
-        N0_Sb = row['N0_Sb']
-        StumpDOB_Aw = row['StumpDOB_Aw']
-        StumpDOB_Sb = row['StumpDOB_Sb']
-        StumpDOB_Sw = row['StumpDOB_Sw']
-        StumpDOB_Pl = row['StumpDOB_Pl']
-        StumpHeight_Aw = row['StumpHeight_Aw']
-        StumpHeight_Sb = row['StumpHeight_Sb']
-        StumpHeight_Sw = row['StumpHeight_Sw']
-        StumpHeight_Pl = row['StumpHeight_Pl']
-        TopDib_Aw = row['TopDib_Aw']
-        TopDib_Sb = row['TopDib_Sb']
-        TopDib_Sw = row['TopDib_Sw']
-        TopDib_Pl = row['TopDib_Pl']
+        SI_bh_Aw = row.at['SI_Aw']
+        SI_bh_Sw = row.at['SI_Sw']
+        SI_bh_Pl = row.at['SI_Pl']
+        SI_bh_Sb = row.at['SI_Sb']
+        N_bh_AwT = row.at['N_Aw']
+        N_bh_SwT = row.at['N_Sw']
+        N_bh_PlT = row.at['N_Pl']
+        N_bh_SbT = row.at['N_Sb']
+        y2bh_Aw = row.at['y2bh_Aw']
+        y2bh_Sw = row.at['y2bh_Sw']
+        y2bh_Sb = row.at['y2bh_Sb']
+        y2bh_Pl = row.at['y2bh_Pl']
+        tage_AwT = row.at['tage_Aw']
+        tage_SwT = row.at['tage_Sw']
+        tage_PlT = row.at['tage_Pl']
+        tage_SbT = row.at['tage_Sb']
+        BA_AwT = row.at['BA_Aw']
+        BA_SwT = row.at['BA_Sw']
+        BA_PlT = row.at['BA_Pl']
+        BA_SbT = row.at['BA_Sb']
+        SDF_Aw0 = row.at['SDF_Aw']
+        SDF_Sw0 = row.at['SDF_Sw']
+        SDF_Pl0 = row.at['SDF_Pl']
+        SDF_Sb0 = row.at['SDF_Sb']
+        N0_Aw = row.at['N0_Aw']
+        N0_Sw = row.at['N0_Sw']
+        N0_Pl = row.at['N0_Pl']
+        N0_Sb = row.at['N0_Sb']
+        StumpDOB_Aw = row.at['StumpDOB_Aw']
+        StumpDOB_Sb = row.at['StumpDOB_Sb']
+        StumpDOB_Sw = row.at['StumpDOB_Sw']
+        StumpDOB_Pl = row.at['StumpDOB_Pl']
+        StumpHeight_Aw = row.at['StumpHeight_Aw']
+        StumpHeight_Sb = row.at['StumpHeight_Sb']
+        StumpHeight_Sw = row.at['StumpHeight_Sw']
+        StumpHeight_Pl = row.at['StumpHeight_Pl']
+        TopDib_Aw = row.at['TopDib_Aw']
+        TopDib_Sb = row.at['TopDib_Sb']
+        TopDib_Sw = row.at['TopDib_Sw']
+        TopDib_Pl = row.at['TopDib_Pl']
 
         BA_Aw0 = BA0_lower_BAT_Aw(BA_AwT)
         BA_Sw0 = BA0_lower_BAT_Sw(BA_SwT)
@@ -319,7 +318,6 @@ def simulate_forwards_df(plot_df, simulation_choice='yes'):
             bhage_PlF = SC_Dict['bhage_Pl']
             SC_PlF = SC_Dict['SC_Pl']
             N_bh_PlT = SC_Dict['N_bh_PlT']
-            '''Ages at time t + 1'''
             logger.debug('Simulating year %d', t)
 
 
@@ -350,10 +348,9 @@ def simulate_forwards_df(plot_df, simulation_choice='yes'):
             else:
                 BA_PlT = 0
 
-            # TODO: should these be t+1?
-            output_DF_Sw.loc[t, 'BA_Sw'] = BA_SwT
-            output_DF_Sb.loc[t, 'BA_Sb'] = BA_SbT
-            output_DF_Pl.loc[t, 'BA_Pl'] = BA_PlT
+            output_DF_Sw.at[t, 'BA_Sw'] = BA_SwT
+            output_DF_Sb.at[t, 'BA_Sb'] = BA_SbT
+            output_DF_Pl.at[t, 'BA_Pl'] = BA_PlT
 
             t += 1
             startTageAwF += 1
@@ -368,18 +365,15 @@ def simulate_forwards_df(plot_df, simulation_choice='yes'):
         )
         #http://stackoverflow.com/questions/25314547/cell-var-from-loop-warning-from-pylint
 
-        output_DF['Gross_Total_Volume_Aw'] = output_DF.apply(
-            lambda x: GrossTotalVolume_Aw(x['BA_Aw'], x['topHeight_Aw']), axis=1
-        )
-        output_DF['Gross_Total_Volume_Sw'] = output_DF.apply(
-            lambda x: GrossTotalVolume_Sw(x['BA_Sw'], x['topHeight_Sw']), axis=1
-        )
-        output_DF['Gross_Total_Volume_Sb'] = output_DF.apply(
-            lambda x: GrossTotalVolume_Sb(x['BA_Sb'], x['topHeight_Sb']), axis=1
-        )
-        output_DF['Gross_Total_Volume_Pl'] = output_DF.apply(
-            lambda x: GrossTotalVolume_Pl(x['BA_Pl'], x['topHeight_Pl']), axis=1
-        )
+        import ipdb
+        for spec in ('Aw', 'Sw', 'Sb', 'Pl'):
+            output_DF['Gross_Total_Volume_%s' % spec] = output_DF.apply(
+                lambda x: GrossTotalVolume_Aw(
+                    x.at['BA_%s' % spec],
+                    x.at['topHeight_%s' % spec],
+                ),
+                axis=1
+            )
 
         output_DF['Gross_Total_Volume_Con'] = output_DF['Gross_Total_Volume_Sw'] \
                                               + output_DF['Gross_Total_Volume_Sb'] \
@@ -388,29 +382,20 @@ def simulate_forwards_df(plot_df, simulation_choice='yes'):
         output_DF['Gross_Total_Volume_Tot'] = output_DF['Gross_Total_Volume_Con'] \
                                               + output_DF['Gross_Total_Volume_Dec']
 
-        output_DF['MerchantableVolumeAw'] = output_DF.apply(
-            lambda x: MerchantableVolumeAw(
-                x['N_bh_AwT'], x['BA_Aw'], x['topHeight_Aw'],
-                StumpDOB_Aw, StumpHeight_Aw, TopDib_Aw, x['Gross_Total_Volume_Aw']
-            ), axis=1
-        )
-        output_DF['MerchantableVolumeSw'] = output_DF.apply(
-            lambda x: MerchantableVolumeSw(
-                x['N_bh_SwT'], x['BA_Sw'], x['topHeight_Sw'],
-                StumpDOB_Sw, StumpHeight_Sw, TopDib_Sw, x['Gross_Total_Volume_Sw']
-            ), axis=1
-        )
-        output_DF['MerchantableVolumeSb'] = output_DF.apply(
-            lambda x: MerchantableVolumeSb(
-                x['N_bh_SbT'], x['BA_Sb'], x['topHeight_Sb'],
-                StumpDOB_Sb, StumpHeight_Sb, TopDib_Sb, x['Gross_Total_Volume_Sb']), axis=1
-        )
-        output_DF['MerchantableVolumePl'] = output_DF.apply(
-            lambda x: MerchantableVolumePl(
-                x['N_bh_PlT'], x['BA_Pl'], x['topHeight_Pl'],
-                StumpDOB_Pl, StumpHeight_Pl, TopDib_Pl, x['Gross_Total_Volume_Pl']
-            ), axis=1
-        )
+        # this could go in the loop above, but is left here for now since
+        # the tests are sensitive to column order
+        for spec in ('Aw', 'Sw', 'Sb', 'Pl'):
+            output_DF['MerchantableVolume%s' % spec] = output_DF.apply(
+                lambda x: MerchantableVolumeAw(
+                    x.at['N_bh_%sT' % spec],
+                    x.at['BA_%s' % spec],
+                    x.at['topHeight_%s' % spec],
+                    x.at['StumpDOB_%s' % spec],
+                    x.at['StumpHeight_%s' % spec],
+                    x.at['TopDib_%s' % spec],
+                    x.at['Gross_Total_Volume_%s' % spec],
+                ), axis=1
+            )
 
         output_DF['MerchantableVolume_Con'] = output_DF['MerchantableVolumeSw'] \
                                               + output_DF['MerchantableVolumeSb'] \
@@ -422,7 +407,7 @@ def simulate_forwards_df(plot_df, simulation_choice='yes'):
         output_dict[plot_id] = output_DF
 
         end = datetime.datetime.now()
-        time = end - start
-        logger.info('plot %s took %f seconds', plot_id, time.total_seconds())
+        duration = end - start
+        logger.info('plot %s took %f seconds', plot_id, duration.total_seconds())
 
     return output_dict
