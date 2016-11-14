36bc44a04e9a2bbed30ce19522bdf608afa3bfea
Jotham Apaloo <jotham.apaloo@tesera.com>
2016-11-14 13:41:50 -0800

    Add reminder for testing gross total volume

---
+++ tests/test_gross_total_volume.py
@@ -0,0 +1,37 @@
+from gypsy import gross_total_volume
+
+def test_pl():
+    # TODO: known value
+    # TODO: 0 as an input
+    # TODO: negative as an input
+    # TODO: scalar and array
+    gross_total_volume.gtv_pl(0, 0)
+    assert False
+
+def test_aw():
+    # TODO: known value
+    # TODO: 0 as an input
+    # TODO: negative as an input
+    # TODO: scalar and array
+    gross_total_volume.gtv_aw(0, 0)
+    assert False
+
+def test_sb():
+    # TODO: known value
+    # TODO: 0 as an input
+    # TODO: negative as an input
+    # TODO: scalar and array
+    gross_total_volume.gtv_sb(0, 0)
+    assert False
+
+def test_sw():
+    # TODO: known value
+    # TODO: 0 as an input
+    # TODO: negative as an input
+    # TODO: scalar and array
+    gross_total_volume.gtv_sw(0, 0)
+    assert False
+
+def test_gross_total_volume():
+    # TODO: test this delegates properly
+    assert False
