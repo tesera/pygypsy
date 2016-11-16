a42c4423748171c5611d82a101f3b70741ff75db
Jotham Apaloo <jotham.apaloo@tesera.com>
2016-11-16 10:45:03 -0800

    Add boilerplate for increment tests
    These would have been very helpful when introducing cython for increment
    functions. It is a lot easier to debug at this fine grained level than
    in wrapping functions.

---
+++ tests/test_increment.py
@@ -0,0 +1,41 @@
+from gypsy import increment
+
+def test_increment_basal_area_pl():
+    args = []
+    expected = None
+    result = increment.increment_basal_area_pl(*args)
+    assert result == expected
+    # TODO: known value
+    # TODO: 0 as an input
+    # TODO: negative as an input
+
+
+def test_increment_basal_area_aw():
+    args = []
+    expected = None
+    result = increment.increment_basal_area_aw(*args)
+    assert result == expected
+    # TODO: known value
+    # TODO: 0 as an input
+    # TODO: negative as an input
+
+
+def test_increment_basal_area_sb():
+    args = []
+    expected = None
+    result = increment.increment_basal_area_sb(*args)
+    assert result == expected
+    # TODO: known value
+    # TODO: 0 as an input
+    # TODO: negative as an input
+
+
+def test_increment_basal_area_sw():
+    args = []
+    expected = None
+    result = increment.increment_basal_area_sw(*args)
+    assert result == expected
+    # TODO: known value
+    # TODO: 0 as an input
+    # TODO: negative as an input
+
