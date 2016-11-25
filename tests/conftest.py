7a2523021eccb81f8052fb3681e970bb5842a0c1
Jotham Apaloo <jotham.apaloo@tesera.com>
2016-11-24 17:24:27 -0800

    Fix build

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ import pytest
 DATA_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data')
 
 
-@pytest.fixture(scope='module')
+@pytest.yield_fixture(scope='module')
 def invalid_cli_config_file():
     path = os.path.join(DATA_DIR, 'invalid_cli_config.txt')
 
