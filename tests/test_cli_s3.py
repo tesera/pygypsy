ece6e96e64a71b4ee5d6499761ffc034b5f6a87c
Jotham Apaloo <jotham.apaloo@tesera.com>
2016-11-29 12:22:37 -0800

    Add simulation test, fix s3 obj exists, & cleanup
    s3 object exists returned bool; removed assert from func name
    define skip if no s3 decorator once
    fix expected filenames with a hack - the problem is that the output dir
    has the s3 prefix on it. it is worth changing the s3 method to params in
    the config/cli, i.e. bucket-name, maybe region. the config generator will
    still need to use the path. later steps can defer to config.

--- tests/test_cli_s3.py
+++ tests/test_cli_s3.py
@@ -1,15 +1,17 @@
 import os
-import shutil
 
 import pytest
 from click.testing import CliRunner
 import botocore
 
 from gypsy.scripts.cli import cli
-from conftest import DATA_DIR
 
 
-def assert_s3_obj_exists(bucket_conn, key):
+SKIP_IF_NO_S3 = pytest.mark.skipif(os.getenv('GYPSY_BUCKET') is None,
+                                   reason="S3 tests are not configured locally")
+
+
+def s3_obj_exists(bucket_conn, key):
     try:
         bucket_conn.Object(key).load()
     except botocore.exceptions.ClientError as e:
@@ -23,11 +25,11 @@ def assert_s3_obj_exists(bucket_conn, key):
     return exists
 
 
-@pytest.mark.skipif(os.getenv('GYPSY_BUCKET') is None,
-                    reason="S3 tests are not configured locally")
+@SKIP_IF_NO_S3
 def test_generate_config(s3_config_output_dir, s3_bucket_conn):
     runner = CliRunner()
     expected_output_path = '%s/%s' % (s3_config_output_dir, 'gypsy-config.json')
+    expected_output_path = expected_output_path.lstrip('s3://hris-gypsy/')
 
     with runner.isolated_filesystem():
         result = runner.invoke(cli, [
@@ -35,54 +37,45 @@ def test_generate_config(s3_config_output_dir, s3_bucket_conn):
         ])
 
         assert result.exit_code == 0
-        assert_s3_obj_exists(s3_bucket_conn, expected_output_path)
+        assert s3_obj_exists(s3_bucket_conn, expected_output_path)
 
-
-@pytest.mark.skipif(os.getenv('GYPSY_BUCKET') is None,
-                    reason="S3 tests are not configured locally")
+@SKIP_IF_NO_S3
 def test_prep(s3_prep_output_dir, s3_bucket_conn):
+    runner = CliRunner()
     expected_output_path = os.path.join(s3_prep_output_dir['out-dir'],
                                         'plot_table_prepped.csv')
-    runner = CliRunner()
+    expected_output_path = expected_output_path.lstrip('s3://hris-gypsy/')
 
     with runner.isolated_filesystem():
         result = runner.invoke(cli, [
             '--output-dir', s3_prep_output_dir['out-dir'],
             'prep', s3_prep_output_dir['data-path']
         ])
-        import ipdb; ipdb.set_trace()
 
         assert result.exit_code == 0
-        assert_s3_obj_exists(s3_bucket_conn, expected_output_path)
+        assert s3_obj_exists(s3_bucket_conn, expected_output_path)
 
 
-@pytest.mark.skipif(os.getenv('GYPSY_BUCKET') is None,
-                    reason="S3 tests are not configured locally")
+@SKIP_IF_NO_S3
 def test_simulate(s3_simulate_output_dir, s3_bucket_conn):
-    output_dir = 'gypsy-output'
-    input_data_path = './data.csv'
+    input_data_path = s3_simulate_output_dir['data-path']
+    output_dir = s3_simulate_output_dir['out-dir']
     expected_files = [
-        os.path.join('simulation-data', '1614424.csv'),
-        os.path.join('simulation-data', '1008174.csv'),
-        'gypsy.log'
+        'simulation-data/1614424.csv',
+        'simulation-data/1008174.csv',
     ]
     expected_output_paths = [
-        os.path.join(output_dir, item) \
-        for item in expected_files
+        '%s/%s' %(output_dir.lstrip('s3://hris-gypsy/'), f) \
+        for f in expected_files
     ]
 
     runner = CliRunner()
 
     with runner.isolated_filesystem():
-        shutil.copy(
-            os.path.join(DATA_DIR, 'raw_standtable_prepped.csv'),
-            input_data_path
-        )
-
         result = runner.invoke(cli, [
             '--output-dir', output_dir,
             'simulate', input_data_path
         ])
 
         assert result.exit_code == 0
-        assert all([os.path.exists(i) for i in expected_output_paths])
+        assert all([s3_obj_exists(s3_bucket_conn, i) for i in expected_output_paths])
