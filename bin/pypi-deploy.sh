f40881113bae92be457bafac735507eebdecb3dd
Jotham Apaloo <jotham.apaloo@tesera.com>
2016-12-05 14:14:48 -0800

    Ignore failures in CI pypi upload

--- bin/pypi-deploy.sh
+++ bin/pypi-deploy.sh
@@ -1,4 +1,8 @@
+# TODO: in future, instead of ignoring failures in pypi upload
+# only build and upload if the latest tag is at HEAD
 . venv/bin/activate
 
 python setup.py sdist
-twine upload -u "${PYPI_USER}" -p "${PYPI_PASSWORD}" dist/*
+# upload to pypi, ignoring failures
+twine upload -u "${PYPI_USER}" -p "${PYPI_PASSWORD}" dist/* || :
+
