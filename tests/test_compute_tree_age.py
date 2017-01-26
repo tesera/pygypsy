cabf42e625d34b5f130b7c9edafb889b8114cceb
Jotham Apaloo <jotham.apaloo@tesera.com>
2017-01-26 10:40:12 -0800

    Parametrize tests tree age functions

--- tests/test_compute_tree_age.py
+++ tests/test_compute_tree_age.py
@@ -1,24 +1,33 @@
-#pylint: disable=missing-docstring
+#pylint: disable=missing-docstring,invalid-name
+
+import pytest
+import numpy.testing as npt
 
 from pygypsy.asaCompileAgeGivenSpSiHt import computeTreeAge
 
-def test_compute_tree_age():
-    assert computeTreeAge(siSp='Aw', treeHt=20, treeSi=15, maxTreeAge=450,
-                          rowIndex=0, printWarnings=True) == 82.544655456378891
-    assert computeTreeAge(siSp='Pl', treeHt=25, treeSi=15, maxTreeAge=450,
-                          rowIndex=0, printWarnings=True) == 153.29542915884193
-    assert computeTreeAge(siSp='Sw', treeHt=25, treeSi=15, maxTreeAge=450,
-                          rowIndex=0, printWarnings=True) == 102.1856914602515
-    assert computeTreeAge(siSp='Sb', treeHt=25, treeSi=15, maxTreeAge=450,
-                          rowIndex=0, printWarnings=True) == 136.17108952176432
 
+@pytest.mark.parametrize("kwargs,expected", [
+    ({'siSp': 'Aw', 'treeHt': 20, 'treeSi': 15}, 82.544),
+    ({'siSp': 'Pl', 'treeHt': 25, 'treeSi': 15}, 153.295),
+    ({'siSp': 'Sw', 'treeHt': 25, 'treeSi': 15}, 102.186),
+    ({'siSp': 'Sb', 'treeHt': 25, 'treeSi': 15}, 136.171)
+])
+def test_compute_tree_age(kwargs, expected):
+    npt.assert_almost_equal(
+        computeTreeAge(**kwargs),
+        expected,
+        decimal=3
+    )
 
-def test_compute_nonconvergent_tree_age():
-    assert computeTreeAge(siSp='Pl', treeHt=25, treeSi=10, maxTreeAge=450,
-                          rowIndex=0, printWarnings=True) == 250
-    assert computeTreeAge(siSp='Aw', treeHt=25, treeSi=1, maxTreeAge=450,
-                          rowIndex=0, printWarnings=True) == 200
-    assert computeTreeAge(siSp='Sb', treeHt=25, treeSi=1, maxTreeAge=450,
-                          rowIndex=0, printWarnings=True) == 450
-    assert computeTreeAge(siSp='Sw', treeHt=25, treeSi=1, maxTreeAge=450,
-                          rowIndex=0, printWarnings=True) == 450
+@pytest.mark.parametrize("kwargs,expected", [
+    ({'siSp': 'Pl', 'treeHt': 25, 'treeSi': 10}, 250),
+    ({'siSp': 'Aw', 'treeHt': 25, 'treeSi': 1}, 200),
+    ({'siSp': 'Sb', 'treeHt': 25, 'treeSi': 1}, 450),
+    ({'siSp': 'Sw', 'treeHt': 25, 'treeSi': 1}, 450)
+])
+def test_compute_nonconvergent_tree_age(kwargs, expected):
+    npt.assert_almost_equal(
+        computeTreeAge(**kwargs),
+        expected,
+        decimal=3
+    )
